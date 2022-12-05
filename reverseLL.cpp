402cbc1dbe7f9eba330da3194ecac2fe67f1da9c
Mohd Faisal <faisal02v@gmail.com>
2022-12-05 15:59:42 +0530

    Update reverseLL.cpp

--- reverseLL.cpp
+++ reverseLL.cpp
@@ -30,3 +30,27 @@ public:
         return prev;
     }
 };
+
+
+
+
+
+
+
+
+
+    //recursive solution
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL||head->next==NULL) return head;
+        
+        ListNode*node=reverseList(head->next);
+        
+        head->next->next=head;
+        head->next=NULL;
+        return node;
+        
+        
+    }
+};
